39becf82336f5422c8b093f31525ae05f9ed4445
brensant <breno_santana_silva@hotmail.com>
2025-09-20 20:34:47 -0300

    cercado inimigos

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -4,12 +4,15 @@ extends Node2D
 @onready var progress_bar_armor: ProgressBar = $CanvasLayer/HBoxContainer/ProgressBarArmor
 @onready var progress_bar_stamina: ProgressBar = $CanvasLayer/HBoxContainer/ProgressBarStamina
 
+@onready var player_knight: CharacterBody2D = $"../PlayerKnight"
 
-
+func _ready():
+	progress_bar_armor.value = player_knight.armor
+	progress_bar_health.value = player_knight.health
+	progress_bar_stamina.value = player_knight.stamina
 
 func _on_player_knight_upate_armor(amount: float) -> void:
-	progress_bar_armor.value = amount
-
+	progress_bar_armor.value = player_knight.armor
 
 func _on_player_knight_upate_health(amount: float) -> void:
 	progress_bar_health.value = amount
